1052345874b6c57a0394f2c511e408145f45987e
Alex <toxiccologist@gmail.com>
2020-05-06 12:49:46 -0300

    filenames

--- 4chan thread downloader.py
+++ 4chan thread downloader.py
@@ -3,36 +3,49 @@ from progressbar import Bar, ProgressBar, Percentage
 
 def getImagesFromThread(thread):
     images = []
+    filenames = []
     d = requests.get(thread).text
+
+    plebs = "4plebs" in thread
+
+    image_filter = '" target="_blank" rel="noreferrer" class="thread_image_link">' if plebs else '<a class="fileThumb" href="//'
+    link = '' if plebs else 'https://'
+
     if "4plebs" in thread:
-        for i in d.split('<a href="https://i.4pcdn.org')[1:]:
-            images.append('https://i.4pcdn.org%s' % i.split('"')[0])
-        for i in d.split('<a href="http://i.4pcdn.org')[1:]:
-            images.append('https://i.4pcdn.org%s' % i.split('"')[0])
-        
+        images = ['%s%s'%(link, post.split('"')[-1]) for post in d.split(image_filter)[:-1]]
+        f_prev = ''
+        for file in d.split('download="')[1:]:
+            f = file.split('"')[0]
+            if f != f_prev:
+                filenames.append(f)
+                f_prev = f
         
     else:
-        posts = d.split('<a class="fileThumb" href="//')[1:]
-        for post in posts:
-            images.append(str('https://%s')%post.split('"')[0])
-            
-    return images
+        images = ['%s%s'%(link, post.split('"')[0]) for post in d.split(image_filter)[1:]]
+        for file in d.split('File: ')[1:]:
+            if '(...)' in file:
+                filenames.append(file.split('<a title="')[1].split('"')[0])
+            else:
+                filenames.append(file.split('</a>')[0].split('>')[-1])
+    return images, filenames
 
-def getImage(image, saveDir):
+def getImage(image, filename, saveDir):
     global saved
     if not os.path.exists(saveDir):
         os.mkdir(saveDir)
-    path = "%s\\%s" % (saveDir, image.split('/')[-1])
+    path = "%s\\%s" % (saveDir, filename)
     if not os.path.exists(path):
         urllib.request.urlretrieve(image, path)
     saved.append(1)
     
 
-def saveImages(images,saveDir):
+def saveImages(images, filenames, saveDir):
     pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=len(images)).start()
-    for image in images:
+    for i in range(len(images)):
+        image = images[i]
+        filename = filenames[i]
         #print("Downloading %s/%s: %s" %(images.index(image),len(images), image))
-        threading.Thread(target=lambda: getImage(image, saveDir)).start()
+        threading.Thread(target=lambda: getImage(image, filename, saveDir)).start()
     while True:
         pbar.update(len(saved))
         if len(images) == len(saved):
@@ -44,10 +57,12 @@ url = str(input("Enter the url: \n"))
 images = []
 saveDir = str(input("Enter savedir: "))
 print('Fetching image links...')
-images = getImagesFromThread(url)
-print('Downloading images')
+images, filenames = getImagesFromThread(url)
+#print(images)
+print(filenames)
+print('Downloading %s images' % len(images))
 try:
-    saveImages(images, saveDir)
+    saveImages(images, filenames, saveDir)
 except KeyboardInterrupt:
     print("Exiting")
 print('Done!')
